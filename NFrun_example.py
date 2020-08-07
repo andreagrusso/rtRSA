14d39e252d5682e72b08244ae5751725c28804c1
Andrea Gerardo Russo <35601556+andreagrusso@users.noreply.github.com>
2020-08-07 12:52:46 +0200

    Again re-ordering

--- NFrun_example.py
+++ NFrun_example.py
@@ -34,6 +34,4 @@ from matplotlib import pyplot as plt
 import matplotlib.lines as mlines
 from psychopy.sound import Sound
 import json
-import sys
-sys.path.insert(1, 'C:/Users/andre/_NeuroImaging/scripts/maastricht/rtRSA/rtrsa')
-from rtrsa import nfrsa, utils
+from ..rtrsa import nfrsa, utils
