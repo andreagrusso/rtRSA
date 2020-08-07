f68f02dbb933bbb09059e80d6238df206d2545bd
Andrea Gerardo Russo <35601556+andreagrusso@users.noreply.github.com>
2020-08-07 15:14:33 +0200

    first creation

--- NFrun_example.py
+++ NFrun_example.py
@@ -55,7 +55,10 @@ rtRSAObj = nfrsa.rtRSA('test',2,'pearson')
 #the config.json file can be created by using one of the class method
 #the file si wirtten after the estimation of the RS
 rtRSAObj.load(os.path.join(wdir,'config.json'))
-
+print('rtRSA ready!\n')
+print('Nr of voxels: ',+ len(rtRSAObj.func_coords))
+print('Base stimuli name:')
+print(rtRSAObj.conditions)
 
 #%%############################################################################
 #                               TBV  interface settings                       #
@@ -63,5 +66,313 @@ rtRSAObj.load(os.path.join(wdir,'config.json'))
 
 #create an instance to access TBV via network plugin
 TBV = tbvnetworkinterface.TbvNetworkInterface('localhost',55555)
+
+win = visual.Window(fullscr=False,color='gray',screen=0,
+                    size=(1024,768),colorSpace='rgb255') 
+
+#creation of the cue for the image
+image = visual.ImageStim(win, 
+                     image=None, 
+                     mask=None, 
+                     units='pix', 
+                     pos=(0.0, 0.0), 
+                     size=600, 
+                     ori=0.0, 
+                     color=(1.0, 1.0, 1.0), 
+                     colorSpace='rgb', 
+                     contrast=1.0, 
+                     opacity=1.0, 
+                     depth=0, 
+                     interpolate=False, 
+                     flipHoriz=False, 
+                     flipVert=False, 
+                     texRes=128, 
+                     name=None, 
+                     autoLog=None, 
+                     maskParams=None)
+
+#creation of the cue for the frame around the image (it is a text stimulus)
+fixation = visual.TextStim(win, 
+                           text='+', 
+                           font='Arial', 
+                           pos=(0.0, 0.0), 
+                           depth=0, 
+                           rgb=None, 
+                           color='white', 
+                           colorSpace='rgb', 
+                           opacity=1.0, 
+                           contrast=1.0, 
+                           units='', 
+                           ori=0.0, 
+                           height=0.15, 
+                           antialias=True, 
+                           bold=False, 
+                           italic=False, 
+                           alignHoriz='center', 
+                           alignVert='center', 
+                           fontFiles=(), 
+                           wrapWidth=None, 
+                           flipHoriz=False, 
+                           flipVert=False, 
+                           languageStyle='LTR', 
+                           name=None, 
+                           autoLog=None)
+
+#clock initialization
+globalClock = core.Clock()
+
+###############################################################################
+#                      THESE VARIABLES DEPEND ON                             #
+#                      THE EXPERIMENTAL DESIGN                               #
+###############################################################################
+
+#path of the stimulus
+stimulus_path = os.path.join(wdir,'sounds/stimuli/cat.wav')
+stimulus = Sound(stimulus_path)
+
+stop_wav= os.path.join(wdir,'sounds/stop.wav')
+stop_stim = Sound(stop_wav)
+
+outdir = os.path.join(wdir,'output')
+
+#condition timings
+baselines = pickle.load(open(os.path.join(wdir,'prt/baselines.pkl'),'rb'))
+tasks = pickle.load(open(os.path.join(wdir,'prt/tasks.pkl'),'rb'))
+feedbacks = pickle.load(open(os.path.join(wdir,'prt/feedbacks.pkl'),'rb'))
+fb_duration = feedbacks[0,1]-feedbacks[0,0] +1
+
+
+#little trick to have the first feedback. TBV4 returns empty tvalues if all
+#predictors have not yet appeared
+feedbacks[0,0] = feedbacks[0,0]+1
+nr_of_trials = feedbacks.shape[0]
+
+#variable to store positions of the stimulus in time
+stimulus_positions = np.empty((nr_of_trials+1,2))
+
+#index of the constrast maps
+idx_ctr = 0
+
+
+###############################################################################
+#                     Create a plot of the base stimuli                       #
+###############################################################################
+
+#setting a basic plot figure
+plt.ion()
+fig = plt.figure()
+
+plt.figure(facecolor='dimgray')
+ax = plt.axes()
+ax.scatter(rtRSAObj.RS_coords[:,0],rtRSAObj.RS_coords[:,1],s=150, c='yellow',
+           edgecolors = 'black')
+# Setting the background color
+ax.set_facecolor('dimgray')
+plt.xticks([])
+plt.yticks([])
+plt.axis('off')
+for label, x, y in zip(conditions, rtRSAObj.RS_coords[:,0],rtRSAObj.RS_coords[:,1]):
+    plt.annotate(label, xy=(x, y), xytext=(20, -20),size=15,
+                 textcoords='offset points', ha='right', va='bottom',
+                 bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=1),
+                 arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0')) 
+
+
+yellow_circle = mlines.Line2D([], [], color='yellow',  linestyle='None', 
+                              marker='o',markeredgecolor='black', markeredgewidth=0.5,
+                          markersize=15, label='Base stimuli')
+red_star = mlines.Line2D([], [], color='red',linestyle='None',  marker='*',
+                         markeredgecolor='black', markeredgewidth=0.5,
+                          markersize=15, label='Current stimulus')
+
+plt.legend(handles=[yellow_circle, red_star],
+    loc='upper center', bbox_to_anchor=(0.5, -0.05),ncol=4)
+
+
+
+
+###############################################################################
+#                             Reading data from TBV                           #
+###############################################################################
+timepoint_timing = []
+
+
+#USEFUl FOR OTHER SCANNERS AND SIMULATIONS
+while '5' not in event.getKeys(['5']):
+    print('Waiting scanner....')
+
+
+# serFmri = serial.Serial('COM1', 57600)
+# prevState = serFmri.getDSR()
+
+# while serFmri.getDSR() == prevState:
+#     print('Waiting scanner....')
+   
+
+globalClock.reset()    
+print("First trigger!")
+
+
+
+  
+
+#it waits until the first time point is processed by TBV to be sure to 
+#read correct data from TBV settings file
+CurrTimePoint = 0
+while TBV.get_current_time_point()[0] < 1:
+    NrOfTimePoints = TBV.get_expected_nr_of_time_points()[0]
+    NrOfROIs = TBV.get_nr_of_rois()[0]
+    print('Waiting TBV....')
+
+
+print("OK let's go! Expected TPs: " + str(NrOfTimePoints))    
+#general loop
+while TBV.get_current_time_point()[0] <= NrOfTimePoints:
+    
+    if CurrTimePoint !=  TBV.get_current_time_point()[0]:
+        timepoint_timing.append([CurrTimePoint ,globalClock.getTime()])
+        #update current timepoint
+        CurrTimePoint = TBV.get_current_time_point()[0]
+        print('Current time point:',str(CurrTimePoint))
+        
+            
+        #looking for a ROI
+        if NrOfROIs == 0:                
+            print('Please add a ROI')
+            NrOfROIs = TBV.get_nr_of_rois()[0]
+        
+        else:
+            #needed to avoid accessing to timepoint -1 (fake) or timepoint 0
+            if CurrTimePoint > 1 :
+                #THE ACTUAL EXPERIMENT STARTS ONLY IF THERE IS A ROI!!!!!!#
+
+                #displaying the results of only one ROI (the first in this case)
+                selected_VOI =  int(config['voi_index'])
+    
+                #coordinates of voxels of the roi
+                coord_roi_voxels = TBV.get_all_coords_of_voxels_of_roi(selected_VOI)[0]
+            
+                
+                #setting the fixation for the baseline
+                if CurrTimePoint in baselines[1:,0]:
+                    fixation.draw()
+                    win.flip()
+                
+                #showing only the frame for the imaginative task
+                elif CurrTimePoint in tasks[:,0]:
+                    print('stimulus')
+                    stimulus.play()
+                    fixation.draw()
+                    win.flip()
+                    
+                elif CurrTimePoint in tasks[:,1]:
+                    stop_stim.play()
+                    print('stop')
+                    
+                #extract the map and plot the current position
+                elif CurrTimePoint in feedbacks[:,0]:
+
+                    #extractiing tvalues from the ROI
+                    #in this experimental paradigm we have only one contrast
+                    tvalues = [TBV.get_map_value_of_voxel(0,coords)[0] 
+                                    for coords in coord_roi_voxels]
+
+                    #estimate nwe stimulus coordinates
+                    stimulus_positions[idx_ctr,:] = rtRSAObj.target_positioning(tvalues)
+                    
+                    
+                    #plotting the new coordinates
+                    if idx_ctr == 0:
+                        print('Contrast:',idx_ctr+1)
+                        plt.scatter(stimulus_positions[idx_ctr,0],
+                                    stimulus_positions[idx_ctr,1], 
+                                    marker = '*',s=200, color = 'red', 
+                                    edgecolors='black')
+                        ax.set_facecolor('dimgray')
+                        plt.xticks([])
+                        plt.yticks([])
+                        plt.axis('off')
+                    else:
+                        print('Contrast:',idx_ctr)
+                        plt.scatter(stimulus_positions[:idx_ctr,0],stimulus_positions[:idx_ctr,1], 
+                                    marker = '*',s=200, color = 'darkgray')
+                        plt.scatter(stimulus_positions[idx_ctr,0],stimulus_positions[idx_ctr,1], 
+                                    marker = '*',s=200, color = 'red', edgecolors='black')
+                        #plotting the trajectory
+                        plt.plot(stimulus_positions[:idx_ctr+1,0],stimulus_positions[:idx_ctr+1,1], '-',
+                                    color = 'green')
+                        ax.set_facecolor('dimgray')
+                        plt.xticks([])
+                        plt.yticks([])
+                        plt.axis('off')
+                            
+                    #save figure
+                    plt.savefig(os.path.join(outdir,'tvals_Trial' + str(idx_ctr)+ '.png'),
+                               facecolor='dimgray', edgecolor='none', dpi=300)
+                    #show the figure()
+                    image.setImage(os.path.join(outdir,'tvals_Trial' + str(idx_ctr)+ '.png'))
+                    image.draw()
+                    win.flip()
+                    core.wait(fb_duration)
+                    
+                    #increment the index of the contrast map
+                    idx_ctr += 1
+                    
+                elif CurrTimePoint == NrOfTimePoints:
+                    print('Last time point!')
+                    #contrast number is fixed for the simulation
+                    #extract tvalues at the corresponding coordinates
+                    tvalues = [TBV.get_map_value_of_voxel(0,coords)[0] 
+                               for coords in coord_roi_voxels]
+                    
+                    #estimate new stimulus coordinates
+                    stimulus_positions[idx_ctr,:] = rtRSAObj.target_positioning(tvalues)
+                    
+                    #plotting the new coordinates
+                    print('Contrast:',idx_ctr+1)
+                    plt.scatter(stimulus_positions[:idx_ctr,0],stimulus_positions[:idx_ctr,1], 
+                                marker = '*',s=200, color = 'darkgray')
+                    for label, x, y in zip(range(idx_ctr),stimulus_positions[:idx_ctr,0],stimulus_positions[:idx_ctr,1]):
+                        plt.annotate(label,xy=(x, y), xytext=(-5, 5))
+                    plt.scatter(stimulus_positions[idx_ctr,0],stimulus_positions[idx_ctr,1], 
+                                marker = '*',s=200, color = 'red', edgecolors='black')
+                    #plotting the trajectory
+                    plt.plot(stimulus_positions[:idx_ctr+1,0],stimulus_positions[:idx_ctr+1,1],'-',
+                                color = 'green')
+                    ax.set_facecolor('dimgray')
+                    plt.xticks([])
+                    plt.yticks([])
+                    plt.axis('off')
+
+                            
+                    #save figure
+                    plt.savefig(os.path.join(outdir,'tvals_Trial' + str(idx_ctr)+ '.png'),
+                               facecolor='dimgray', edgecolor='none', dpi=300)
+                    #show the figure()
+                    image.setImage(os.path.join(outdir,'tvals_Trial' + str(idx_ctr)+ '.png'))
+                    image.draw()
+                    win.flip()
+                    core.wait(fb_duration)
+                    
+                    break
+
+                else:
+                    fixation.draw()
+                    win.flip()
+                       
+
+                    
+              
+plt.close()
+
+win.close()
+
+
+with open(os.path.join(outdir,'timepoint_timing_incrementalGLM.txt'), 'w') as f:
+    for item in timepoint_timing:
+        f.write("%s\n" % item) 
+f.close()            
+
+
 
     
