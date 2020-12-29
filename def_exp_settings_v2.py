5494cada409330da5d43dcf09605d42b607a4048
Andrea Gerardo Russo <35601556+andreagrusso@users.noreply.github.com>
2020-12-29 16:53:07 +0100

    Update experimental setting define
    Now it is possible to change esily the length of the two conditions

--- def_exp_settings_v2.py
+++ def_exp_settings_v2.py
@@ -23,36 +23,55 @@ settings = dict()
 #%%############################################################################
 
 #first block limits
-baseline_duration = 20
-task_duration = 40
+baseline_duration, task_duration = 20,40
 
 
 block_duration = baseline_duration + task_duration
 
-baselines = np.array([1, baseline_duration]).reshape(1,-1)
-tasks = np.array([baseline_duration+1,baseline_duration + task_duration]).reshape(1,-1)
+baselineonset = np.array([1,1 + baseline_duration])
+offset = onset + baseline_duration-1
 
-
+init_onset = onset
+init_offset = offset
 
 n_rip = 10
 
 for i in range(1,n_rip):
-    baselines = np.vstack((baselines,baselines[0,:]+i*block_duration))
-    tasks = np.vstack((tasks,tasks[0,:]+i*block_duration))
+    onset = np.append(onset,init_onset+i*block_duration)
+    offset = np.append(offset,init_offset+i*block_duration)
 
 #additional last baseline
-baselines = np.vstack((baselines,baselines[-1,:]+block_duration))
+onset = np.append(onset,offset[-1]+1)
+offset = np.append(offset,onset[-1]+19)
 
 
 
-feedbacks = np.array([np.arange(tasks[i,0]+7,tasks[i,1],2)
+
+baseline_index = np.arange(0,len(onset),2)
+baselines = np.vstack((onset[baseline_index],offset[baseline_index])).T
+task_index = np.arange(1,len(onset),2)
+tasks = np.vstack((onset[task_index],offset[task_index])).T
+
+feedbacks = np.array([np.arange(tasks[i,0]+5,tasks[i,1],2)
                          for i in range(len(tasks))]).reshape(-1,1)
 
 #%%############################################################################
 
 conditions = ['cat','chair','dog','hammer']
 
-  
+#non incremental prt
+for im in conditions:
+    im_name = im.split('.')[0]    
+    protocol = prt.StimulationProtocol(experiment_name="RSA Pilot 1st run " + im_name, 
+                                       time_units="Volumes")
+    #baseline
+    protocol.add_condition(prt.Condition("Baseline", baselines,colour=[0, 0, 255]))
+    #task                                     
+    for i in range(tasks.shape[0]):
+        protocol.add_condition(prt.Condition(im_name + '_' + str(i), tasks[i,:].reshape((1,2)),colour=[255, i, 0]))
+
+        
+    protocol.save(os.path.join(outdir,'continuous', im_name +'_RSA_individual_task.prt'))  
 
 #incremental prt    
 for im in conditions:
@@ -79,22 +98,3 @@ pickle.dump(tasks,f)
 
 f=open(os.path.join(outdir,'continuous','feedbacks.pkl'),'wb')
 pickle.dump(feedbacks,f)
-
-
-#non incremental prt
-for im in conditions:
-    im_name = im.split('.')[0]    
-    protocol = prt.StimulationProtocol(experiment_name="RSA Pilot 1st run " + im_name, 
-                                       time_units="Volumes")
-    #baseline
-    len_baseline = baselines.shape[0]
-    baselines[0,:] = np.array([1,8])
-    baselines = np.insert(baselines,2,np.array([11,20])).reshape(len_baseline+1,2)
-    protocol.add_condition(prt.Condition("Baseline", baselines,colour=[0, 0, 255]))
-    #task                                     
-    for i in range(tasks.shape[0]):
-        tmp_task = np.vstack((np.array([9,10]),tasks[i,:]))
-        protocol.add_condition(prt.Condition(im_name + '_' + str(i), tmp_task, colour=[255, i, 0]))
-
-        
-    protocol.save(os.path.join(outdir,'continuous', im_name +'_RSA_individual_task.prt'))
