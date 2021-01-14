484b4cc12b722b93a2332823ac92688e7c56aae5
Andrea Gerardo Russo <35601556+andreagrusso@users.noreply.github.com>
2021-01-14 15:41:13 +0100

    Re-ordering

--- experiment/def_exp_settings.py
+++ experiment/def_exp_settings.py
@@ -23,8 +23,8 @@ settings = dict()
 #%%############################################################################
 
 #first block limits
-baseline_duration = 30
-task_duration = 30
+baseline_duration = 20
+task_duration = 20
 
 
 block_duration = baseline_duration + task_duration
@@ -50,14 +50,14 @@ feedbacks = np.array([np.arange(tasks[i,0]+7,tasks[i,1],2)
 
 #%%############################################################################
 
-conditions = ['cat','chair','dog','hammer']
+conditions = ['gatto','sedia','cane','martello']
 
   
 
 #incremental prt    
 for im in conditions:
     im_name = im.split('.')[0]    
-    protocol = prt.StimulationProtocol(experiment_name="RSA Pilot 1st run " + im_name, 
+    protocol = prt.StimulationProtocol(experiment_name="rtRSA " + im_name, 
                                        time_units="Volumes")
     #baselines
     protocol.add_condition(prt.Condition("Baseline", baselines,colour=[0, 0, 255]))
@@ -81,20 +81,20 @@ f=open(os.path.join(outdir,'continuous','feedbacks.pkl'),'wb')
 pickle.dump(feedbacks,f)
 
 
-#non incremental prt
-for im in conditions:
-    im_name = im.split('.')[0]    
-    protocol = prt.StimulationProtocol(experiment_name="RSA Pilot 1st run " + im_name, 
-                                       time_units="Volumes")
-    #baseline
-    len_baseline = baselines.shape[0]
-    baselines[0,:] = np.array([1,8])
-    baselines = np.insert(baselines,2,np.array([11,20])).reshape(len_baseline+1,2)
-    protocol.add_condition(prt.Condition("Baseline", baselines,colour=[0, 0, 255]))
-    #task                                     
-    for i in range(tasks.shape[0]):
-        tmp_task = np.vstack((np.array([9,10]),tasks[i,:]))
-        protocol.add_condition(prt.Condition(im_name + '_' + str(i), tmp_task, colour=[255, i, 0]))
+# #non incremental prt
+# for im in conditions:
+#     im_name = im.split('.')[0]    
+#     protocol = prt.StimulationProtocol(experiment_name="RSA Pilot 1st run " + im_name, 
+#                                        time_units="Volumes")
+#     #baseline
+#     len_baseline = baselines.shape[0]
+#     baselines[0,:] = np.array([1,8])
+#     baselines = np.insert(baselines,2,np.array([11,20])).reshape(len_baseline+1,2)
+#     protocol.add_condition(prt.Condition("Baseline", baselines,colour=[0, 0, 255]))
+#     #task                                     
+#     for i in range(tasks.shape[0]):
+#         tmp_task = np.vstack((np.array([9,10]),tasks[i,:]))
+#         protocol.add_condition(prt.Condition(im_name + '_' + str(i), tmp_task, colour=[255, i, 0]))
 
         
-    protocol.save(os.path.join(outdir,'continuous', im_name +'_RSA_individual_task.prt'))
+#     protocol.save(os.path.join(outdir,'continuous', im_name +'_RSA_individual_task.prt'))
